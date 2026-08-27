5889f8e8d0d34fa97b68e39c831363fa1b991918
phzadas <guterph@gmail.com>
2026-08-27 16:55:52 -0300

    Add files via upload

---
+++ desafio3.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+soma = a + b
+print(soma)
+
+subtracao = a - b
+print(subtracao)
+
+multiplicacao = a * b
+print(multiplicacao)
---
+++ desafio4.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+divisao_integra = a // b
+print(divisao_integra)
+
+divisao_normal = a / b
+print(divisao_normal)
